4979cec04602e54a24a18d8948fc5fd635c7f3f4
Tera Coder <TerableCoder@gmail.com>
2020-04-04 02:57:11 -0400

    cleanup

--- index.js
+++ index.js
@@ -16,18 +16,6 @@ module.exports = function AnywhereXD(mod) {
         //"bank":"2020",
         "hustler":"2046",
         "bell":"2045"};
-    /*const templateIdToGameId = {
-        "9903": "3518437209324318",
-        "9805": "3518437209372133",
-        "9904": "3518437209337218",
-        "9905": "3518437209335804",
-        "2058": "3518437209367528",
-        "2059": "3518437209394117",
-        "2109": "3518437209367634",
-        "2019": "3518437209392877",
-        "2020": "3518437209379780",
-        "2046": "3518437209383315",
-        "2045": "3518437209405223"};*/
     const templateIdToType = {
         "9903": "9",
         "9805": "93",
@@ -84,120 +72,6 @@ module.exports = function AnywhereXD(mod) {
             name: "",
             data: Buffer.from(templateIdToData[tid])
         })
-
-
-        /*
-        if(requestType == "angler"){ // works
-            let npc = NPCs.find(o => o.templateId == 9904);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 20,
-                gameId: npc.gameId,
-                button: 16095,
-                name: "",
-                data: Buffer.from([ 223, 62, 0, 0 ])
-            })
-        } else if(requestType == "merchantf"){
-            let npc = NPCs.find(o => o.templateId == 9903);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 9,
-                gameId: npc.gameId,
-                button: 16094,
-                name: "",
-                data: Buffer.from([ 222, 62, 0, 0 ])
-            })
-        } else if(requestType == "merchant"){
-            let npc = NPCs.find(o => o.templateId == 2019);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 9,
-                gameId: npc.gameId,
-                button: 70310,
-                name: "",
-                data: Buffer.from([ 166, 18, 1, 0 ])
-            })
-        } else if(requestType == "fish"){
-            let npc = NPCs.find(o => o.templateId == 9905);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 93,
-                gameId: npc.gameId,
-                button: 1000,
-                name: "",
-                data: Buffer.from([ 232, 3, 0, 0 ])
-            })
-        } else if(requestType == "hustler"){
-            let npc = NPCs.find(o => o.templateId == 2046);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 9,
-                gameId: npc.gameId,
-                button: 251,
-                name: "",
-                data: Buffer.from([ 251, 0, 0, 0 ])
-            })
-        } else if(requestType == "bell"){ // works
-            let npc = NPCs.find(o => o.templateId == 2045);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 50,
-                gameId: npc.gameId,
-                button: 141,
-                name: "",
-                data: Buffer.from([ 141, 0, 0, 0 ])
-            })
-        } else if(requestType == "vang1"){
-            let npc = NPCs.find(o => o.templateId == 2059);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 49,
-                gameId: npc.gameId,
-                button: 6090,
-                name: "",
-                data: Buffer.from([ 202, 23, 0, 0 ])
-            })
-        } else if(requestType == "vang2"){
-            let npc = NPCs.find(o => o.templateId == 2058);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 49,
-                gameId: npc.gameId,
-                button: 609,
-                name: "",
-                data: Buffer.from([ 97, 2, 0, 0 ])
-            })
-        } else if(requestType == "spec"){
-            let npc = NPCs.find(o => o.templateId == 2109);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 9,
-                gameId: npc.gameId,
-                button: 250,
-                name: "",
-                data: Buffer.from([ 250, 0, 0, 0 ])
-            })
-        } else if(requestType == "crate"){ // find stuff for 
-            let npc = NPCs.find(o => o.templateId == 9805);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 93,
-                gameId: npc.gameId,
-                button: "1001",
-                name: "",
-                data: Buffer.from([ 233, 3, 0, 0 ])
-            })
-        } else if(requestType == "bank"){
-            let npc = NPCs.find(o => o.templateId == 2020);
-            if(!npc) return;
-            mod.toServer('C_REQUEST_CONTRACT', 2, {
-                type: 26,
-                gameId: npc.gameId,
-                button: 0,
-                name: "",
-                data: Buffer.from([ 0, 0, 0, 0 ])
-            })
-        }*/
     }
 
     
@@ -213,11 +87,6 @@ module.exports = function AnywhereXD(mod) {
 			mod.saveSettings();
 		}
     });
-
-    /*mod.hook('S_DESPAWN_NPC', 3, e => {
-		let i = NPCs.findIndex(o => o.gameId == e.gameId);
-		if(i >= 0) NPCs.splice(i, 1);
-	});*/
 
     cmd.add(['anyw'], { 
 		$none(){
